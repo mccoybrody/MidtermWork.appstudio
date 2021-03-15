ad2cfdc4dcfc5ceb220f11c5eac7c044ea875250
Brody McCoy <79420042+mccoybrody@users.noreply.github.com>
2021-03-15 15:54:20 -0500

    calculate

---
+++ forms/animals/animals.js
@@ -0,0 +1,11 @@
+/*
+let myAnimals = ["dog", "cat", "horse", "meercat"]
+
+let animals = prompt("Please enter the name of an animal")
+
+let allAnimal = animals.toLowerCase()
+
+myAnimals.push(allAnimal)
+
+alert(`The last animal is a/an is ${myAnimals[myAnimals.length-1]}`)
+*/
